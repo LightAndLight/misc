2b2b5e69a2cfa283b6a470305910a342a643f510
Isaac Elliott <isaac@Jenells-MacBook-Pro.local>
2024-12-07 16:19:57 +1000

    Add MacOS stdio

--- 20241207-rust-crossplatform-test/src/stdio.rs
+++ 20241207-rust-crossplatform-test/src/stdio.rs
@@ -12,6 +12,20 @@ mod linux {
     }
 }
 
+#[cfg(target_os = "macos")]
+mod macos {
+    use libc::FILE;
+
+    // https://stackoverflow.com/questions/28362994/where-is-my-stdio-h-in-mac
+    // https://stackoverflow.com/questions/47584399/how-to-get-the-corresponding-ctype-for-stdout-on-os-x-using-libc-dylib
+    #[link(name = "c")]
+    extern "C" {
+        pub static mut __stdinp: *mut FILE;
+        pub static mut __stdoutp: *mut FILE;
+        pub static mut __stderrp: *mut FILE;
+    }
+}
+
 #[cfg(target_os = "windows")]
 mod windows {
     use libc::FILE;
@@ -29,12 +43,17 @@ pub unsafe fn stdin() -> *mut FILE {
         linux::stdin
     }
 
+    #[cfg(target_os = "macos")]
+    {
+        macos::__stdinp
+    }
+
     #[cfg(target_os = "windows")]
     {
         windows::__acrt_iob_func(0)
     }
 
-    #[cfg(not(any(target_os = "linux", target_os = "windows")))]
+    #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
     {
         compile_error!("stdin: unsupported target_os")
     }
@@ -46,12 +65,17 @@ pub unsafe fn stdout() -> *mut FILE {
         linux::stdout
     }
 
+    #[cfg(target_os = "macos")]
+    {
+        macos::__stdoutp
+    }
+
     #[cfg(target_os = "windows")]
     {
         windows::__acrt_iob_func(1)
     }
 
-    #[cfg(not(any(target_os = "linux", target_os = "windows")))]
+    #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
     {
         compile_error!("stdout: unsupported target_os")
     }
@@ -63,12 +87,17 @@ pub unsafe fn stderr() -> *mut FILE {
         linux::stderr
     }
 
+    #[cfg(target_os = "macos")]
+    {
+        macos::__stderrp
+    }
+
     #[cfg(target_os = "windows")]
     {
         windows::__acrt_iob_func(2)
     }
 
-    #[cfg(not(any(target_os = "linux", target_os = "windows")))]
+    #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
     {
         compile_error!("stderr: unsupported target_os")
     }
